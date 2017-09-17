c0e0a00193c2e1d60b67d7ff8bdf3b9fbfe90986
tiagodevezas <tiagodevezas@gmail.com>
2017-09-17 16:28:24 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package TPDL2018
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class( 'montserrat w-100' ); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tpdl2018' ); ?></a>
+
+	<header id="masthead" class="site-header ph6-l ph3 pv4-l pv2">
+		<div class="site-branding w-100 flex items-center-ns">
+			<div class="logo w-10 mw4">
+				<img src="<?php echo get_template_directory_uri() . '/images/logo_v1.svg'; ?>" />
+			</div>
+			<div class="w-90">
+				<p class="ml4 dib tr tdpl-light-blue fw2">
+					porto <br>
+					portugal <br>
+					sep. 18 - 21 2018
+				</p>
+				<p class="fw8 tdpl-blue dib">
+					22nd international conference <br>
+					on theory and practice <br>
+					of digital libraries
+				</p>
+			</div>
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+			<?php
+			endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tpdl2018' ); ?></button>
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content ph6-ns ph3">
