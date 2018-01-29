d041d8a5a6119410f3a9616af7c1b8bb95496d94
tiagodevezas <tiagodevezas@gmail.com>
2018-01-29 16:01:03 +0000

    prepare footer for keynote speakers

--- footer.php
+++ footer.php
@@ -13,22 +13,70 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer bg-tdpl-light-beige mt0 tpdl-blue flex justify-center">
-		<div class="site-info mw9">
-			<div id="patron" class="ph3 pv1 mt3 mb3 br3">
-				<h1 class="mt0">Conference Patron</h1>
+	<footer id="colophon" class="site-footer bg-tdpl-light-beige mt0 tpdl-blue">
+		<div class="site-info center mw9 flex flex-wrap justify-center">
+			<div id="patron" class="ph3 pv1 mt3 mb3 br3 w-100 w-50-m w-25-l">
+				<h1 class="mt0 tc">Conference Patron</h1>
 				<hr class="mw3 bb bw1 b--black-10">
 				<a href="conference-patron/">
 					<article class="mw5 center pb3 pb4-ns mb3">
 					  <div class="tc">
-					    <img src="http://localhost/wordpress/wp-content/uploads/2018/01/patron.jpg" class="br-100 h4 w4 dib" title="Maria Fernanda Rollo">
+					    <img src="http://localhost/wordpress/wp-content/uploads/2018/01/maria_fernanda_rollo.jpg" class="br-100 h4 w4 dib" title="Maria Fernanda Rollo">
 					    <h1 class="f4">Maria Fernanda Rollo</h1>
 					  </div>
 					  <p class="lh-copy measure center f6 black-70">
-					    Dr. Maria Fernanda Rollo, State Secretary of Science, Technology and Higher Education, kindly accepted the Organizing Committee's invitation to be the Patron of TPDL 2018.
+					    Professor Maria Fernanda Rollo, the Secretary of State for Science, Technology and Higher Education, has kindly accepted to be the Patron of TPDL 2018. Professor Maria Fernanda Rollo is a researcher in Contemporary History, with a vast contribution to Digital Humanities. Her work as a Secretary of State is actively promoting all aspects of Open Knowledge and Open Science.
 					  </p>
 					</article>
 				</a>
+			</div>
+
+			<div id="keynote-speakers" class="ph3 pv1 mt3 mb3 br3 w-75-l w-100-m tc">
+				<h1 class="mt0 c">Keynote Speakers</h1>
+				<hr class="mw3 bb bw1 b--black-10">
+				<div class="speakers w-100 flex flex-wrap">
+
+						<article class="center pb3 pb4-ns mb3 w-100 w-50-m w-25-l">
+						  <div class="tc">
+						    <img src="http://via.placeholder.com/135x135" class="br-100 h4 w4 dib" title="name">
+						    <h1 class="f4">Name of Speaker</h1>
+						  </div>
+						  <p class="lh-copy measure center f6 black-70">
+						    Short bio.
+						  </p>
+						</article>
+
+						<article class="center pb3 pb4-ns mb3 w-100 w-50-m w-25-l">
+						  <div class="tc">
+						    <img src="http://via.placeholder.com/135x135" class="br-100 h4 w4 dib" title="name">
+						    <h1 class="f4">Name of Speaker</h1>
+						  </div>
+						  <p class="lh-copy measure center f6 black-70">
+						    Short bio.
+						  </p>
+						</article>
+
+						<article class="center pb3 pb4-ns mb3 w-100 w-50-m w-25-l">
+						  <div class="tc">
+						    <img src="http://via.placeholder.com/135x135" class="br-100 h4 w4 dib" title="name">
+						    <h1 class="f4">Name of Speaker</h1>
+						  </div>
+						  <p class="lh-copy measure center f6 black-70">
+						    Short bio.
+						  </p>
+						</article>
+
+						<article class="center pb3 pb4-ns mb3 w-100 w-50-m w-25-l">
+						  <div class="tc">
+						    <img src="http://via.placeholder.com/135x135" class="br-100 h4 w4 dib" title="name">
+						    <h1 class="f4">Name of Speaker</h1>
+						  </div>
+						  <p class="lh-copy measure center f6 black-70">
+						    Short bio.
+						  </p>
+						</article>
+
+				</div>
 			</div>
 
 			<!-- <h1>Hello</h1> -->
