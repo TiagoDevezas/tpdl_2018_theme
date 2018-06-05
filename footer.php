70be6f71a50659965d55c26d8067592c0ad1a387
Tiago Devezas <tiagodevezas@iMac-de-Tiago.local>
2018-06-05 15:42:49 +0100

    sync with deployed theme version

--- footer.php
+++ footer.php
@@ -18,44 +18,57 @@
 			<div id="patron" class="ph3 pv1 mt3 mb3 br3 w-100 w-50-m w-25-l">
 				<h1 class="mt0 tc mb2">Conference Patron</h1>
 				<hr class="mt0 mw3 bb bw1 b--black-10 mb1">
-				<a href="http://www.tpdl.eu/tpdl2018/conference-patron/" class="link dim">
+				<a href="conference-patron" class="link dim">
 					<article class="mw5 center pv3 pb4-ns mb3">
 					  <div class="tc">
 					    <img src="http://www.tpdl.eu/tpdl2018/wp-content/uploads/2018/01/maria_fernanda_rollo.jpg" class="br-100 h4 w4" title="Maria Fernanda Rollo">
 					    <h1 class="f4">Maria Fernanda Rollo</h1>
 					  </div>
-					  <p class="lh-copy measure center f6 black-70">
+					  <p class="lh-copy measure tj f6 black-70">
 					    Professor Maria Fernanda Rollo, the Secretary of State for Science, Technology and Higher Education, has kindly accepted to be the Patron of TPDL 2018. Professor Maria Fernanda Rollo is a researcher in Contemporary History, with a vast contribution to Digital Humanities. Her work as a Secretary of State is actively promoting all aspects of Open Knowledge and Open Science.
 					  </p>
 					</article>
 					</a>
 			</div>
-
+			
 			<div id="keynote-speakers" class="ph3 pv1 mt3 mb3 br3 w-75-l tc flex flex-column">
 				<h1 class="mt0 tc mb2">Keynote Speakers</h1>
 				<hr class="w3 bb bw1 b--black-10 mb1 mt0">
 				<div class="speakers w-100 flex flex-wrap flex-auto">
-						
-					<a href="http://www.tpdl.eu/tpdl2018/medha-devare/" class="link dim pv3 pb3 pb4-ns mb3 w-100 w-50-m w-50-l">
-						<article class="center">
-						  <div class="tc">
-						    <img src="http://www.tpdl.eu/tpdl2018/wp-content/uploads/2018/04/medha_devare.jpg" class="br-100 h4 w4 dib" title="Medha Devare">
-						    <h1 class="f4">Medha Devare</h1>
-						  </div>
-						  <p class="lh-copy measure center f6 black-70 ph4">
-						    Medha Devare is Senior Research Fellow with the International Food Policy Research Institute (IFPRI) and leads efforts to organize data across the CGIAR System’s 15 Centers through its Big Data Platform. She is a Cropping Systems Agronomist and has led Open Data and food security projects.
-						  </p>
-						</article>
-					</a>
 
-					<a href="http://www.tpdl.eu/tpdl2018/natalia-manola/" class="link dim pv3 pb3 pb4-ns mb3 w-100 w-50-m w-50-l">
+
+					<a href="http://www.tpdl.eu/tpdl2018/natalia-manola/" class="link dim pv3 pb3 pb4-ns mb3 w-100 w-50-m w-33-l">
 						<article class="center">
 						  <div class="tc">
 						    <img src="http://www.tpdl.eu/tpdl2018/wp-content/uploads/2018/04/natalia_manola.jpg" class="br-100 h4 w4 dib" title="Natalia Manola">
 						    <h1 class="f4">Natalia Manola</h1>
 						  </div>
-						  <p class="lh-copy measure center f6 black-70 ph4">
+						  <p class="lh-copy measure tj f6 black-70 ph4">
 						    Natalia Manola is a research associate in the University of Athens, Department of Informatics and Telecommunications and the “Athena” Technology and Innovation Research Center. She has served as the managing director of OpenAIRE and OpenMinTeD and is currently a member of EC’s Open Science Policy Platform. Her research interests include e-Infrastructure development and management, scientific data management, data curation and validation, text and data mining, complex data visualization, and research analytics.
+						  </p>
+						</article>
+					</a>
+
+					<a href="http://www.tpdl.eu/tpdl2018/herbert-van-de-sompel/" class="link dim pv3 pb3 pb4-ns mb3 w-100 w-50-m w-33-l">
+						<article class="center">
+						  <div class="tc">
+						    <img src="http://www.tpdl.eu/tpdl2018/wp-content/uploads/2018/04/herbert_van_de_sompel.jpg" class="br-100 h4 w4 dib" title="Herbert Van De Sompel">
+						    <h1 class="f4">Herbert Van de Sompel</h1>
+						  </div>
+						  <p class="lh-copy measure tj f6 black-70 ph4">
+						    Herbert Van de Sompel is an Information Scientist at the Los Alamos National Laboratory. Herbert has played a major role in creating the Open Archives Initiative Protocol for Metadata Harvesting, the Open Archives Initiative Object Reuse & Exchange specifications, the OpenURL Framework for Context-Sensitive Services, the SFX linking server, the bX scholarly recommender service, info URI, Web Annotation, ResourceSync, Memento "time travel for the Web", Robust Links, and Signposting the Scholarly Web.
+						  </p>
+						</article>
+					</a>
+						
+					<a href="http://www.tpdl.eu/tpdl2018/medha-devare/" class="link dim pv3 pb3 pb4-ns mb3 w-100 w-50-m w-33-l">
+						<article class="center">
+						  <div class="tc">
+						    <img src="http://www.tpdl.eu/tpdl2018/wp-content/uploads/2018/04/medha_devare.jpg" class="br-100 h4 w4 dib" title="Medha Devare">
+						    <h1 class="f4">Medha Devare</h1>
+						  </div>
+						  <p class="lh-copy measure tj f6 black-70 ph4">
+						    Medha Devare is Senior Research Fellow with the International Food Policy Research Institute (IFPRI) and leads its Big Data Platform efforts to organize data across the CGIAR System’s 15 Centers. She has led CGIAR food security projects in South Asia, and its Open Access/Open Data Initiative. Medha also has expertise in data management and semantic web tools; while at Cornell University she was instrumental in the development of VIVO, a semantic web application for representing scholarship.
 						  </p>
 						</article>
 					</a>
@@ -70,24 +83,10 @@
 						    Short bio.
 						  </p>
 						</article>
-					</a>
-
-					<a href="#" class="link dim pv3 pb3 pb4-ns mb3 w-100 w-50-m w-25-l">
-						<article class="center">
-						  <div class="tc">
-						    <img src="http://via.placeholder.com/135x135" class="br-100 h4 w4 dib" title="name">
-						    <h1 class="f4">Name of Speaker</h1>
-						  </div>
-						  <p class="lh-copy measure center f6 black-70">
-						    Short bio.
-						  </p>
-						</article>
 					</a> -->
 
 				</div>
 			</div>
-			
-
 
 
 <!-- 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tpdl2018' ) ); ?>"><?php
@@ -101,6 +100,7 @@
 			?> -->
 
 		</div><!-- .site-info -->
+
 
 		<div id="sponsors" class="bg-white">
 			<div class="sponsors-wrapper mw9 pt3 flex flex-wrap center justify-center w-100">
@@ -127,7 +127,6 @@
 						    </a>
 						</article>
 
-
 					</div>
 				</div>
 
@@ -151,6 +150,7 @@
 
 			</div>
 		</div>
+
 
 	</footer><!-- #colophon -->
 	<div id="credits" class="w-100 bg-near-black pv3">
